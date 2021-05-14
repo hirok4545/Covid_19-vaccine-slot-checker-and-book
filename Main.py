df2f9ac3532a87f668df783ea4d7f40006da30b8
Hirokjyoti Deka <hdeka731@gmail.com>
2021-05-14 22:26:47 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,14 +9,14 @@ from time import sleep
 browser_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
 
 
-
+pincode=''
 count=0
 
 while True:
     from datetime import date
     today = date.today()
     date = today.strftime("%d-%m-2021")
-    URL =('https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin?pincode=786602&date='+date)
+    URL =('https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin?pincode='+ pincode + '&date=' + date)
     response = requests.get(URL, headers=browser_header)
     for data in response.json()['centers']:
             for datac in data ['sessions']:
@@ -29,7 +29,6 @@ while True:
                 count += 1
                 if x >0:
                     print(str(dk) + " " + 'Vaccine Slot  Available' + " at" + name )
-                    sendmessage('phoneno',"Slot Available")
                 elif x == 0:
                      print("Tracked " + str(count)+ " time of center id " +str(dk) + " " + 'Vaccine slot Not  available' + " " + name )
                      sleep(2)
